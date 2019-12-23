b2641a747cfcfe15e150633dad53f174343b05a1
jjb915 <wjdwoqls00@naver.com>
2019-12-23 15:39:44 +0900

    6588 add comment

--- 6588.Goldbach's_conjecture.c
+++ 6588.Goldbach's_conjecture.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+
+problem website
+https://www.acmicpc.net/problem/6588
+
+This question's main points are
+1. How do we derive the fact that the input number
+ is divided into two prime numbers or not.
+2. How do we find out specific number is prime number.
+
+This is my solution to first question
+1. Determine if both a and b are decimal, increasing a from 1 to the input/2. (b = input - a)
+
+to secend question
+2. use is_prime function
+ if you want more information
+ use this reference https://en.wikipedia.org/wiki/Primality_test
+ and find pseudocode
+ 
+*/
+
 int is_prime(n) {
 	if (n <= 3)
 		return n > 1;
@@ -30,7 +51,7 @@ int main() {
 	
 	for (i=0; i < count; ++i) {
 		int input = arr[i];
-		for (a=0; a <= input/2; ++a) {
+		for (a=1; a <= input/2; ++a) {
 			b = input - a;
 			if (is_prime(a) && is_prime(b)) {
 				printf("%d = %d + %d\n", input, a, b);
