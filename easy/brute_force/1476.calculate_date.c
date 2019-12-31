3e901ba11663be257a439f3e56ad2083322b46f6
jjb915 <wjdwoqls00@naver.com>
2019-12-31 11:05:54 +0900

    1476 calculate_date

---
+++ easy/brute_force/1476.calculate_date.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int E, S, M; // 15, 28, 19
+	int e=1, s=1, m=1, year=1;
+	scanf("%d %d %d", &E, &S, &M);
+	
+	while (1) {
+		if (e==E && s==S && m==M) break;
+		++e; ++s; ++m; ++year;
+		if (e==16) e=1;
+		if (s==29) s=1;
+		if (m==20) m=1;
+	}
+	
+	printf("%d", year);
+	
+	return 0;
+}
